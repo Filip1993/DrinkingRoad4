2855634b3fd06dfb6c4a18a35e70a0cfc6b106a0
Filip <filip.kesteli@gmail.com>
2016-05-29 18:37:59 +0200

    Initial commit

---
+++ app/src/main/java/com/filipkesteli/drinkingroad4/MainActivity.java
@@ -0,0 +1,116 @@
+package com.filipkesteli.drinkingroad4;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    //private variables that represents views
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+    private android.support.v7.widget.Toolbar mToolbar;
+    private ActionBarDrawerToggle mDrawerToggle;
+    //private variables that represents business logic
+    private FragmentManager mFragmentManager;
+    private FragmentTransaction mFragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initWidgets();
+        setupFragmentManager();
+        setupNavigationDrawer();
+    }
+
+    private void initWidgets() {
+        /**
+         *Setup the DrawerLayout and NavigationView
+         */
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView = (NavigationView) findViewById(R.id.shitstuff);
+        /**
+         * Setup Drawer Toggle of the Toolbar
+         */
+        mToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.app_name,
+                R.string.app_name);
+        //Kljucna komanda - sad mogu stavljati ikone na Toolbar preko menu-a!!!
+        setSupportActionBar(mToolbar);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+    }
+
+    private void setupFragmentManager() {
+        /**
+         * Lets inflate the very first fragment
+         * Here , we are inflating the SwipeTabsFragment as the first Fragment
+         */
+        //uzmemo trenutni fragment...
+        //unutar kontejnera, tj. FragmentLayout-a stavimo SwipeTabsFragment
+        mFragmentManager = getSupportFragmentManager();
+        mFragmentManager
+                .beginTransaction()
+                .replace(R.id.containerView, new SwipeTabsFragment())
+                .commit();
+
+        /*mFragmentTransaction = mFragmentManager.beginTransaction();
+        mFragmentTransaction.replace(R.id.containerView, new SwipeTabsFragment()).commit();*/
+    }
+
+    private void setupNavigationDrawer() {
+        /**
+         * Setup click events on the Navigation View Items.
+         */
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                //ugasimo drawer ako kliknemo na neki od itema
+                mDrawerLayout.closeDrawers();
+                if (menuItem.getItemId() == R.id.nav_item_sent) {
+                    mFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.containerView, new SentFragment())
+                            .commit();
+                }
+                if (menuItem.getItemId() == R.id.nav_item_inbox) {
+                    mFragmentManager
+                            .beginTransaction()
+                            .replace(R.id.containerView, new SwipeTabsFragment())
+                            .commit();
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater -> Returns a MenuInflater with this context
+        MenuInflater menuInflater = getMenuInflater();
+        //We are inflating menu layout with menu variable
+        menuInflater.inflate(R.menu.menu_toolbar, menu);
+        //onCreateOptionsMenu returns true if menu is to be displayed
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_home:
+                break;
+            case R.id.action_face:
+                break;
+        }
+        return true;
+    }
+}
---
+++ app/src/main/java/com/filipkesteli/drinkingroad4/SwipeTabsFragment.java
@@ -0,0 +1,104 @@
+package com.filipkesteli.drinkingroad4;
+
+/**
+ * Created by Filip on 22.5.2016..
+ */
+
+//SwipeTabsFragment je fragment koji se inflate-a kad se pritisne neki gumb na
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SwipeTabsFragment extends Fragment {
+
+    public static TabLayout tabLayout;
+    public static ViewPager viewPager;
+    public static final int NUMBER_OF_ITEMS = 3 ;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        /**
+         *Inflate tab_layout and setup Views.
+         */
+        View view =  inflater.inflate(R.layout.tab_layout,null);
+        tabLayout = (TabLayout) view.findViewById(R.id.tabs);
+        viewPager = (ViewPager) view.findViewById(R.id.viewpager);
+
+        /**
+         *Set an Apater for the View Pager
+         */
+        viewPager.setAdapter(new ViewPagerAdapter(getChildFragmentManager()));
+
+        /**
+         * Now , this is a workaround ,
+         * The setupWithViewPager dose't works without the runnable .
+         * Maybe a Support Library Bug .
+         */
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+            }
+        });
+        return view;
+    }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        //array of names of fragments:
+        private String[] tabs = {"Start", "Settings", "Rules"};
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        /**
+         * Return fragment with respect to Position .
+         */
+
+        @Override
+        public Fragment getItem(int position)
+        {
+            switch (position){
+                case 0 :
+                    return new StartFragment();
+                case 1 :
+                    return new SettingsFragment();
+                case 2 :
+                    return new RulesFragment();
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+            return NUMBER_OF_ITEMS;
+        }
+
+        /**
+         * This method returns the title of the tab according to the position.
+         */
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            switch (position){
+                case 0 :
+                    return tabs[0];
+                case 1 :
+                    return tabs[1];
+                case 2 :
+                    return tabs[2];
+            }
+            return null;
+        }
+    }
+}
