a0c18a425667665edb66be26adf035988b8dcadd
Filip <filip.kesteli@gmail.com>
2016-05-29 19:46:52 +0200

    MapsActivity ADDED

--- app/src/main/java/com/filipkesteli/drinkingroad4/StartFragment.java
+++ app/src/main/java/com/filipkesteli/drinkingroad4/StartFragment.java
@@ -1,11 +1,13 @@
 package com.filipkesteli.drinkingroad4;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -13,6 +15,7 @@ import android.view.ViewGroup;
  */
 public class StartFragment extends Fragment {
 
+    private Button btnStart;
 
     public StartFragment() {
         // Required empty public constructor
@@ -23,7 +26,26 @@ public class StartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false);
+        View v = inflater.inflate(R.layout.fragment_start, container, false);
+
+        initWidgets(v);
+        setupListeners();
+
+        return v;
+    }
+
+    private void initWidgets(View v) {
+        btnStart = (Button) v.findViewById(R.id.btnStart);
+    }
+
+    private void setupListeners() {
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
